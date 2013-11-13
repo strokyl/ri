3e2e456092e6367980129a2880ab4ff81e46ce33
modeprobe <adrien.gareau@gmail.com>
2013-11-13 10:19:49 +0100

    ajout d'un package index et d'une classe index pour les differentes methodes a effectuer pour l'indexation

---
+++ src/com/insa/rila/index/Index.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.insa.rila.index;
+
+/**
+ *
+ * @author adrien
+ */
+public class Index {
+
+    public static String toLower (String content)
+    {
+        return content.toLowerCase();
+    }
+
+}
