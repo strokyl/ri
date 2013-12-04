87448a8dc96a4ce5c083c4dcd5c2c51daa293156
Luc DUZAN <luc.duzan@gmail.com>
2013-12-04 18:10:54 +0100

    Modification calcule tf idf, de merde

--- src/com/insa/rila/db/TermeParagraphe.java
+++ src/com/insa/rila/db/TermeParagraphe.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.insa.rila.db;
 
 import java.util.HashSet;
@@ -13,88 +12,104 @@ import java.util.Set;
  * @author strokyl
  */
 public class TermeParagraphe {
-    private float tf;
-    private float tf_robertson;
-    private float tf_idf;
-    private final Paragraphe paragraphe;
-    private final Terme terme;
-    private final Set<Apparition> apparitions;
-    private int id;
 
+	private float tf;
+	private float tf_robertson;
+	private float tf_idf;
+	private final Paragraphe paragraphe;
+	private final Terme terme;
+	private final Set<Apparition> apparitions;
+	private int id;
 
+	/**
+	 * Pas util pour la db, juste util pour le calcul de tf
+	 */
+	private final Set<ApparitionType> apparitionTypes;
 
-    /**
-     * Créer un terme_paragraphe qui s'ajoute automatiquement
-     * dans le terme et le paragraphe correspondant
-     * @param paragraphe
-     * @param terme
-     */
-    public TermeParagraphe(Paragraphe paragraphe, Terme terme) {
-        this.tf=0f;
-        this.paragraphe = paragraphe;
-        this.terme = terme;
-        this.apparitions = new HashSet<Apparition>();
-        terme.addTermeParagraphes(this);
-        paragraphe.addTermeParagraphes(this);
-    }
+	/**
+	 * Créer un terme_paragraphe qui s'ajoute automatiquement dans le terme
+	 * et le paragraphe correspondant
+	 *
+	 * @param paragraphe
+	 * @param terme
+	 */
+	public TermeParagraphe(Paragraphe paragraphe, Terme terme) {
+		this.tf = 0f;
+		this.paragraphe = paragraphe;
+		this.terme = terme;
+		this.apparitions = new HashSet<Apparition>();
+		this.apparitionTypes = new HashSet<ApparitionType>();
+		terme.addTermeParagraphes(this);
+		paragraphe.addTermeParagraphes(this);
+	}
 
-    public Paragraphe getParagraphe() {
-        return paragraphe;
-    }
+	public Paragraphe getParagraphe() {
+		return paragraphe;
+	}
 
-    public void addApparition(Apparition apparition) {
-        this.apparitions.add(apparition);
-    }
+	public void addApparition(Apparition apparition) {
+		this.apparitions.add(apparition);
+	}
 
-    public Set<Apparition> getApparition()
-    {
-        return this.apparitions;
-    }
+	public void addApparitionTypes(ApparitionType apparitionType) {
+		this.apparitionTypes.add(apparitionType);
+	}
 
-    public Terme getTerme() {
-        return terme;
-    }
+	public Set<ApparitionType> getApparitionTypes() {
+		return apparitionTypes;
+	}
 
-    public float getTf() {
-        return tf;
-    }
+	public boolean hasApparitionType(ApparitionType apparitionType) {
+		return apparitionTypes.contains(apparitionType);
+	}
 
-    public void setTf(float tf) {
-        this.tf = tf;
-    }
+	public Set<Apparition> getApparition() {
+		return this.apparitions;
+	}
 
-     public float getTf_idf() {
-        return tf_idf;
-    }
+	public Terme getTerme() {
+		return terme;
+	}
 
-    public void setTf_idf(float tf_idf) {
-        this.tf_idf = tf_idf;
-    }
+	public float getTf() {
+		return tf;
+	}
 
-    public float getTf_robertson() {
-        return tf_robertson;
-    }
+	public void setTf(float tf) {
+		this.tf = tf;
+	}
 
-    public void setTf_robertson(float tf_robertson) {
-        this.tf_robertson = tf_robertson;
-    }
+	public float getTf_idf() {
+		return tf_idf;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public void setTf_idf(float tf_idf) {
+		this.tf_idf = tf_idf;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public float getTf_robertson() {
+		return tf_robertson;
+	}
 
+	public void setTf_robertson(float tf_robertson) {
+		this.tf_robertson = tf_robertson;
+	}
 
-    public boolean isTermeCorres(Terme ter)
-    {
-       
-       return ter.getId()== this.terme.getId();
-    }
+	public int getId() {
+		return id;
+	}
 
-    public void incTf(float ponderation) {
-        this.tf+=ponderation;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public boolean isTermeCorres(Terme ter) {
+
+		return ter.getId() == this.terme.getId();
+	}
+
+	public void incTf() {
+		//this.tf+=ponderation;
+		this.tf += 1;
+	}
 }
--- src/com/insa/rila/index/Index.java
+++ src/com/insa/rila/index/Index.java
@@ -39,227 +39,234 @@ import org.tartarus.snowball.util.StemmerException;
  */
 public class Index {
 
-    public static final String WORD_DELIMITER_REGEXP = "(\\s|[-!?,.;:()_/\\\\'|\"])+";
-    private static Set<String> stopWords;
+	public static final String WORD_DELIMITER_REGEXP = "(\\s|[-!?,.;:()_/\\\\'|\"])+";
+	private static Set<String> stopWords;
 
-    public static Set<String> getStopWords() throws FileNotFoundException, IOException {
-        if (stopWords == null) {
-            stopWords = new HashSet<String>();
-            String word;
-            Scanner sc = new Scanner(new File("stopliste.txt"), "UTF-8");
-            while (sc.hasNextLine()) {
-                word = sc.nextLine().trim().toLowerCase();
-                stopWords.add(word);
-            }
+	public static Set<String> getStopWords() throws FileNotFoundException, IOException {
+		if (stopWords == null) {
+			stopWords = new HashSet<String>();
+			String word;
+			Scanner sc = new Scanner(new File("stopliste.txt"), "UTF-8");
+			while (sc.hasNextLine()) {
+				word = sc.nextLine().trim().toLowerCase();
+				stopWords.add(word);
+			}
 
-            sc.close();
-        }
+			sc.close();
+		}
 
-        return stopWords;
-    }
+		return stopWords;
+	}
 
-    public static String toLower(String content) {
-        return content.toLowerCase();
-    }
+	public static String toLower(String content) {
+		return content.toLowerCase();
+	}
 
-    public static List<String> getToken(String content) {
-        List<String> result = new LinkedList<String>();
+	public static List<String> getToken(String content) {
+		List<String> result = new LinkedList<String>();
 
-        for (String s : content.split(WORD_DELIMITER_REGEXP)) {
-            result.add(s);
-        }
+		for (String s : content.split(WORD_DELIMITER_REGEXP)) {
+			result.add(s);
+		}
 
-        return result;
-    }
+		return result;
+	}
 
-    public static List<String> removeStopwords(List<String> words) throws FileNotFoundException, IOException {
+	public static List<String> removeStopwords(List<String> words) throws FileNotFoundException, IOException {
 
-        Set<String> stopWords = getStopWords();
-        List<String> newList = new LinkedList<String>();
-        for (int i = 0; i < words.size(); i++) {
-            if (!stopWords.contains(words.get(i))) {
-                newList.add(words.get(i));
-            }
+		Set<String> stopWords = getStopWords();
+		List<String> newList = new LinkedList<String>();
+		for (int i = 0; i < words.size(); i++) {
+			if (!stopWords.contains(words.get(i))) {
+				newList.add(words.get(i));
+			}
 
-        }
-        return newList;
+		}
+		return newList;
 
         // A faire sauver la position du mot 
+	}
 
-    }
+	/**
+	 * Take a word and only keep is racine For example : discrimination ->
+	 * discriminer : discrinaient -> discriminaient
+	 *
+	 * @param words
+	 */
+	public static List stemming(List<String> words) {
+		List<String> test = new LinkedList<String>();
+		for (int i = 0; i < words.size(); i++) {
+			try {
+				test.add(FrenchSnowballStemmerFactory.getInstance().process(words.get(i)));
+			} catch (StemmerException ex) {
+				Logger.getLogger(Index.class.getName()).log(Level.SEVERE, null, ex);
+			}
+		}
+		return test;
+	}
 
-    /**
-     * Take a word and only keep is racine
-     * For example : discrimination -> discriminer
-     *             : discrinaient   -> discriminaient
-     * @param words
-     */
-    public static List stemming(List<String> words) {
-        List<String> test = new LinkedList<String>();
-        for (int i = 0; i < words.size(); i++) {
-            try {
-                test.add(FrenchSnowballStemmerFactory.getInstance().process(words.get(i)));
-            } catch (StemmerException ex) {
-                Logger.getLogger(Index.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-        return test;
-    }
+	/**
+	 * Replace all accent into correspondig ascii caractere in a token list
+	 *
+	 * @param words
+	 */
+	public static List<String> asciiFolding(List<String> words) {
+		List<String> result = new LinkedList<String>();
 
-    /**
-     * Replace all accent into correspondig ascii caractere in a token list
-     * @param words
-     */
-    public static List<String> asciiFolding(List<String> words) {
-        List<String> result = new LinkedList<String>();
+		for (String word : words) {
+			result.add(asciiFoldingOnWord(word));
+		}
 
-        for (String word : words) {
-            result.add(asciiFoldingOnWord(word));
-        }
+		return result;
+	}
 
-        return result;
-    }
+	public static String asciiFoldingOnWord(String word) {
+		String result = Normalizer.normalize(word, Normalizer.Form.NFD);
+		result = result.replaceAll("[^\\p{ASCII}]", "");
+		return result;
+	}
 
-    public static String asciiFoldingOnWord(String word) {
-        String result = Normalizer.normalize(word, Normalizer.Form.NFD);
-        result = result.replaceAll("[^\\p{ASCII}]", "");
-        return result;
-    }
+	/**
+	 * Aggregate duplicate token from a list
+	 *
+	 * @param words list of token
+	 * @return a map that associate a token to the number of time it waw
+	 * appearing in the input list
+	 */
+	public static DbInRam createDbInRam(List<ParagraphBrut> words) throws FileNotFoundException, IOException {
+		DbInRam result = new DbInRam();
+		Map<String, Terme> mapTermes = new HashMap<String, Terme>();
+		Set<Paragraphe> setParagraphes = new HashSet<Paragraphe>();
+		Map<String, Document> mapDocuments = new HashMap<String, Document>();
+		Document doc;
+		Paragraphe paraDb;
+		Map<Terme, TermeParagraphe> mapTermPara;
 
-    /**
-     * Aggregate duplicate token from a list
-     * @param words list of token
-     * @return a map that associate a token to the number of time it waw appearing
-     * in the input list
-     */
-    public static DbInRam createDbInRam(List<ParagraphBrut> words) throws FileNotFoundException, IOException {
-        DbInRam result = new DbInRam();
-        Map<String, Terme> mapTermes = new HashMap<String, Terme>();
-        Set<Paragraphe> setParagraphes = new HashSet<Paragraphe>();
-        Map<String, Document> mapDocuments = new HashMap<String, Document>();
-        Document doc;
-        Paragraphe paraDb;
-        Map<Terme, TermeParagraphe> mapTermPara;
+		for (ParagraphBrut paraBrut : words) {
+			paraBrut.computeToken();
 
-        for (ParagraphBrut paraBrut : words) {
-            paraBrut.computeToken();
+			// Create or Update Document
+			doc = mapDocuments.get(paraBrut.getXmlUrl());
+			if (doc == null) {
+				doc = new Document(paraBrut.getXmlUrl());
+				result.addDocuments(doc);
+				mapDocuments.put(paraBrut.getXmlUrl(), doc);
+			}
 
-            // Create or Update Document
-            doc = mapDocuments.get(paraBrut.getXmlUrl());
-            if (doc == null) {
-                doc = new Document(paraBrut.getXmlUrl());
-                result.addDocuments(doc);
-                mapDocuments.put(paraBrut.getXmlUrl(), doc);
-            }
+			//Create Doc
+			mapTermPara = new HashMap<Terme, TermeParagraphe>();
+			paraDb = new Paragraphe(paraBrut.getXmlPath(), doc);
+			result.addParagraphe(paraDb);
+			updateSets(result, mapTermes, paraDb, mapTermPara, paraBrut.getTokenTitre(), ApparitionType.TITRE);
+			updateSets(result, mapTermes, paraDb, mapTermPara, paraBrut.getTokenParagraph(), ApparitionType.PARAGRAPHE);
+			updateSets(result, mapTermes, paraDb, mapTermPara, paraBrut.getTokenSousTitre(), ApparitionType.SOUS_TITRE);
+			updateSets(result, mapTermes, paraDb, mapTermPara, paraBrut.getTokenDescription(), ApparitionType.DESCRIPTION);
 
-            //Create Doc
-            mapTermPara = new HashMap<Terme, TermeParagraphe>();
-            paraDb = new Paragraphe(paraBrut.getXmlPath(), doc);
-            result.addParagraphe(paraDb);
-            updateSets(result, mapTermes, paraDb, mapTermPara, paraBrut.getTokenTitre(), ApparitionType.TITRE);
-            updateSets(result, mapTermes, paraDb, mapTermPara, paraBrut.getTokenParagraph(), ApparitionType.PARAGRAPHE);
-            updateSets(result, mapTermes, paraDb, mapTermPara, paraBrut.getTokenSousTitre(), ApparitionType.SOUS_TITRE);
-            updateSets(result, mapTermes, paraDb, mapTermPara, paraBrut.getTokenDescription(), ApparitionType.DESCRIPTION);
-        }
+			for (TermeParagraphe termeParagraphe : mapTermPara.values()) {
+				for(ApparitionType apparitionType : ApparitionType.values()) {
+					if(termeParagraphe.hasApparitionType(apparitionType)) {
+						termeParagraphe.setTf(termeParagraphe.getTf()*apparitionType.getPonderation());
+					}
+				}
+			}
+		}
 
+		return result;
+	}
 
+	private static void updateSets(DbInRam result, Map<String, Terme> mapTermes,
+		Paragraphe paraDB, Map<Terme, TermeParagraphe> mapTermPara, List<String> tokens,
+		ApparitionType apparitionType) {
 
+		Map<String, Apparition> mapApparition = new HashMap<String, Apparition>();
+		Terme terme;
+		TermeParagraphe termeParagraphe;
+		Apparition apparition;
+		Position position;
+		int index = 0;
+		paraDB.incSommeApparitionTermes(tokens.size());
+		for (String token : tokens) {
+			terme = mapTermes.get(token);
+			if (terme == null) {
+				terme = new Terme(token);
+				result.addTermes(terme);
+				mapTermes.put(token, terme);
+			}
 
+			termeParagraphe = mapTermPara.get(terme);
+			if (termeParagraphe == null) {
+				termeParagraphe = new TermeParagraphe(paraDB, terme);
+				result.addTermeParagraphes(termeParagraphe);
+				mapTermPara.put(terme, termeParagraphe);
+			}
+			termeParagraphe.addApparitionTypes(apparitionType);
 
-        return result;
-    }
+			apparition = mapApparition.get(token);
+			if (apparition == null) {
+				apparition = new Apparition(termeParagraphe, apparitionType);
+				result.addApparition(apparition);
+				mapApparition.put(token, apparition);
+			}
 
-    private static void updateSets(DbInRam result, Map<String, Terme> mapTermes,
-            Paragraphe paraDB,Map<Terme, TermeParagraphe> mapTermPara ,List<String> tokens,
-            ApparitionType apparitionType) {
+			position = new Position(apparition, index);
+			termeParagraphe.incTf();
+			result.addPosition(position);
 
-        Map<String, Apparition> mapApparition = new HashMap<String, Apparition>();
-        Terme terme;
-        TermeParagraphe termeParagraphe;
-        Apparition apparition;
-        Position position;
-        int index = 0;
-        paraDB.incSommeApparitionTermes(tokens.size());
-        for (String token : tokens) {
-            terme = mapTermes.get(token);
-            if (terme == null) {
-                terme = new Terme(token);
-                result.addTermes(terme);
-                mapTermes.put(token, terme);
-            }
-
-            termeParagraphe = mapTermPara.get(terme);
-            if (termeParagraphe == null) {
-                termeParagraphe = new TermeParagraphe(paraDB, terme);
-                result.addTermeParagraphes(termeParagraphe);
-                mapTermPara.put(terme, termeParagraphe);
-            }
-
-            apparition = mapApparition.get(token);
-            if (apparition == null) {
-                apparition = new Apparition(termeParagraphe, apparitionType);
-                result.addApparition(apparition);
-                mapApparition.put(token, apparition);
-            }
-
-            position = new Position(apparition, index);
-            termeParagraphe.incTf(apparitionType.getPonderation());
-            result.addPosition(position);
-
-            index++;
-        }
+			index++;
+		}
 
         //result.calculTfMax();
+	}
 
-        
-    }
+	/**
+	 *
+	 * @param text
+	 * @return return a map with all token found in the text and there
+	 * occurence, token only contain ascii password and are stemmed,
+	 * stopword are removed
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+	public static List<String> getTokenList(String text) throws FileNotFoundException, IOException {
+		String lower = toLower(text);
+		List<String> words = removeStopwords(getToken(lower));
+		List<String> tokens = asciiFolding(stemming(words));
+		return tokens;
+	}
 
-    /**
-     *
-     * @param text
-     * @return return a map with all token found in the text and there occurence, token only contain ascii password
-     * and are stemmed, stopword are removed
-     * @throws FileNotFoundException
-     * @throws IOException
-     */
-    public static List<String> getTokenList(String text) throws FileNotFoundException, IOException {
-        String lower = toLower(text);
-        List<String> words = removeStopwords(getToken(lower));
-        List<String> tokens = asciiFolding(stemming(words));
-        return tokens;
-    }
+	/**
+	 * Prends un ensemble de map token occurence et le merge avec des coeffs
+	 * spécifique
+	 *
+	 * @param coeffs
+	 * @param maps
+	 * @return
+	 */
+	public static Map<String, Float> mergeMapOfToken(float[] coeffs, Map<String, Integer>... maps) {
+		if (coeffs.length != maps.length) {
+			throw new RuntimeException("coeffs size should be equals to maps size");
+		}
 
-    /**
-     * Prends un ensemble de map token occurence et le merge avec des coeffs spécifique
-     * @param coeffs
-     * @param maps
-     * @return
-     */
-    public static Map<String, Float> mergeMapOfToken(float[] coeffs, Map<String, Integer>... maps) {
-        if (coeffs.length != maps.length) {
-            throw new RuntimeException("coeffs size should be equals to maps size");
-        }
+		Map<String, Float> result = new HashMap<String, Float>();
 
-        Map<String, Float> result = new HashMap<String, Float>();
+		float currentCoeff;
+		float value;
+		Map<String, Integer> currentMap;
+		for (int i = 0; i < coeffs.length; i++) {
+			currentCoeff = coeffs[i];
+			currentMap = maps[i];
 
-        float currentCoeff;
-        float value;
-        Map<String, Integer> currentMap;
-        for (int i = 0; i < coeffs.length; i++) {
-            currentCoeff = coeffs[i];
-            currentMap = maps[i];
+			for (String token : currentMap.keySet()) {
+				value = currentMap.get(token).floatValue() * currentCoeff;
+				if (result.containsKey(token)) {
+					value += result.get(token);
+				}
 
-            for (String token : currentMap.keySet()) {
-                value = currentMap.get(token).floatValue() * currentCoeff;
-                if (result.containsKey(token)) {
-                    value += result.get(token);
-                }
+				result.put(token, value);
+			}
 
-                result.put(token, value);
-            }
+		}
 
-        }
-
-        return result;
-    }
+		return result;
+	}
 }
